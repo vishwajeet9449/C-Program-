43ca06396525c489a0492851b54914d90a307232
Vishwajeet Singh <vishwajeetsingh9449@gmail.com>
2024-11-25 00:06:47 +0530

    Add files via upload
    Adding Two Program In this Repo.
    Thank You For Visit

---
+++ Calculate_SI_.c
@@ -0,0 +1,25 @@
+// Author - Vishwajeet Singh 
+// Learning From the book Let Us C by Yashavant Kanetkar
+
+#include<stdio.h>
+
+// This is a function called main()
+
+int main()
+{
+    // Declaring Variable 
+    int p,t;
+    float r,si;
+
+// Given Value 
+    p = 1000;
+    t = 3;
+    r = 8.5;
+
+// Formula For Simple Intrest
+    si = (p * r * t)/100;
+
+printf("%f", si);
+return 0;
+
+}
---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello Vishwajeet Singh");
+    return 0;
+}
